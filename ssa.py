4583e09a36ad4fedaebb6ecf08bba4b6bef266d2
Jonathan Tran <jtran2718@gmail.com>
2022-02-19 17:42:22 -0500

    add var renaming for to-ssa

--- ssa.py
+++ ssa.py
@@ -3,8 +3,11 @@ import click
 import sys
 import json
 
+from numpy import s_
+
 from bril_core_constants import *
-from cfg import TERMINATORS, form_cfg_succs_preds, form_blocks, form_block_dict, join_blocks
+from cfg import SUCCS, TERMINATORS, form_cfg_succs_preds, form_blocks, form_block_dict, join_blocks
+from dominator_utilities import build_dominance_tree
 
 
 def gen_var_name():
@@ -50,6 +53,61 @@ def ssa_to_bril(program):
         new_func_instrs = func_from_ssa(func["instrs"])
         func["instrs"] = new_func_instrs
     return program
+
+
+def insert_phi(func):
+    pass
+
+
+def rename(block_map, block_name, stack, cfg, dom_tree):
+    pushed_names = dict()
+
+    block = block_map[block_name]
+    for instr in block:
+        if ARGS in instr:
+            old_args = instr[ARGS]
+            new_args = []
+            for old in old_args:
+                (v, i) = stack[old]
+                var = f"{v}_{i}"
+                new_args.append(var)
+            instr[ARGS] = new_args
+        if DEST in instr:
+            dst = instr[DEST]
+            (v, i) = stack[dst]
+            new_name = f"{v}_{i + 1}"
+            instr[DEST] = new_name
+            stack[dst].append((v, i + 1))
+
+            if dst in pushed_names:
+                pushed_names[dst].append(new_name)
+            else:
+                pushed_names[dst] = [new_name]
+
+    for s in cfg[block][SUCCS]:
+        s_phi_nodes = []
+        for instr in s:
+            if OP in instr and instr[OP] == PHI:
+                s_phi_nodes.append(instr)
+        for p in s_phi_nodes:
+            p_args = p[ARGS]
+            new_p_args = []
+            for a in p_args:
+                if a in stack:
+                    (v, i) = stack[a]
+                    new_var = f"{v}_{i}"
+                    new_p_args.append(new_var)
+                else:
+                    new_p_args.append(a)
+            p[ARGS] = new_p_args
+
+    for new_block_name in dom_tree[block_name]:
+        rename(block_map, new_block_name, stack, cfg, dom_tree)
+
+    for var, new_names in pushed_names.items():
+        while new_names != []:
+            _ = new_names.pop()
+            stack[var].pop()
 
 
 def func_to_ssa(func):
