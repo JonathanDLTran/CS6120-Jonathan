343d299b57b107654abaa23e1d283e36daada5e4
Jonathan Tran <jtran2718@gmail.com>
2022-02-28 20:30:14 -0500

    add licm li detection

--- licm.py
+++ licm.py
@@ -4,8 +4,9 @@ import click
 import sys
 import json
 
-from cfg import form_cfg_w_blocks, insert_into_cfg, join_cfg
-from dominator_utilities import get_natural_loops
+from cfg import form_cfg_w_blocks, insert_into_cfg, join_cfg, INSTRS, SUCCS, PREDS
+from reaching_definitions import reaching_defs_func
+from dominator_utilities import get_natural_loops, build_dominance_tree
 from bril_core_constants import *
 
 
@@ -43,24 +44,125 @@ def insert_preheaders(natural_loops, cfg):
     return preheadermap
 
 
-def identify_loop_invariant_instrs(loop_instrs):
+def identify_li_recursive(cfg, dominance_tree, reaching_definitions, loop_instrs, loop_blocks, basic_block, instrs_invariant_map, var_invariant_map):
+    (in_dict, _) = reaching_definitions
+    instrs = cfg[basic_block][INSTRS]
+    for instr in instrs:
+        # constants
+        if VALUE in instr and DEST in instr:
+            instrs_invariant_map[id(instr)] = LOOP_INVARIANT
+            var_invariant_map[instr[DEST]] = LOOP_INVARIANT
+        # consider instructions with arguments
+        elif ARGS in instr and DEST in instr:
+            args = instr[ARGS]
+            dst = instr[DEST]
+            is_loop_invariant = True
+            for x in args:
+                # condition 1: Arguments are defined by
+                # values defined outside of loop
+                x_reaches_from_outside_loop = True
+                for block in cfg:
+                    for (_, var) in in_dict[block]:
+                        if var == dst and block in loop_blocks:
+                            x_reaches_from_outside_loop = False
+                            break
+                    if not x_reaches_from_outside_loop:
+                        break
+                if x_reaches_from_outside_loop:
+                    continue
+
+                # condition 2: Arguments are defined exactly once
+                # inside the loop, and the argument itself was
+                # marked as loop invariant
+                x_def_counter = 0
+                for loop_instr in loop_instrs:
+                    if DEST in loop_instr:
+                        other_dst = loop_instr[DEST]
+                        if other_dst == dst:
+                            x_def_counter += 1
+                if x_def_counter == 1 and var_invariant_map[x]:
+                    continue
+
+                is_loop_invariant = False
+                break
+
+            if is_loop_invariant:
+                instrs_invariant_map[id(instr)] = LOOP_INVARIANT
+                var_invariant_map[dst] = LOOP_INVARIANT
+            else:
+                instrs_invariant_map[id(instr)] = NOT_LOOP_INVARIANT
+                var_invariant_map[dst] = NOT_LOOP_INVARIANT
+        else:
+            instrs_invariant_map[id(instr)] = NOT_LOOP_INVARIANT
+
+    children = dominance_tree[basic_block]
+    for c in children:
+        if c in loop_blocks:
+            instrs_invariant_map = identify_li_recursive(
+                cfg, dominance_tree, reaching_definitions, loop_instrs, loop_blocks, c, instrs_invariant_map, deepcopy(var_invariant_map))
+
+    return instrs_invariant_map
+
+
+def identify_loop_invariant_instrs(cfg, loop_blocks, loop_instrs, loop_header, reaching_definitions, dominance_tree):
+    """
+    For a Given Loop, identify those instructions in the loop that are loop invariant
+    """
+    assert loop_header in loop_blocks
+
+    # mark all insdtructions as not loop invariant
+    instrs_invariant_map = OrderedDict()
+    for loop_instr in loop_instrs:
+        instrs_invariant_map[id(loop_instr)] = NOT_LOOP_INVARIANT
+
     continue_while = True
     while continue_while:
-        for instr in loop_instrs:
-            if ARGS in instr:
-                args = instr[ARGS]
-                for x in args:
-                    pass
+        old_loop_instrs = deepcopy(loop_instrs)
+
+        var_invariant_map = OrderedDict()
+        for loop_instr in loop_instrs:
+            if DEST in loop_instr:
+                var_invariant_map[loop_instr[DEST]] = NOT_LOOP_INVARIANT
+        instrs_invariant_map = identify_li_recursive(cfg, dominance_tree, reaching_definitions, loop_instrs, loop_blocks, loop_header,
+                                                     instrs_invariant_map, var_invariant_map)
+
+        if loop_instrs == old_loop_instrs:
+            continue_while = False
+
+    return instrs_invariant_map
 
 
 def move_loop_invariant_instrs():
     pass
 
 
+def loop_licm(natural_loop, cfg, preheadermap, reaching_definitions, dominance_tree):
+    # Grab the instructions in a loop
+    (loop_blocks, _, header) = natural_loop
+    loop_instrs = []
+    for block_name in cfg:
+        if block_name in loop_blocks:
+            loop_instrs += cfg[block_name][INSTRS]
+
+    loop_instrs_map = identify_loop_invariant_instrs(
+        cfg, loop_blocks, loop_instrs, header, reaching_definitions, dominance_tree)
+
+    for instr in loop_instrs:
+        if id(instr) in loop_instrs_map:
+            print(f"{id(instr)} | {instr} | {loop_instrs_map[id(instr)]}")
+
+
 def func_licm(func):
     natural_loops = get_natural_loops(func)
     cfg = form_cfg_w_blocks(func)
     preheadermap = insert_preheaders(natural_loops, cfg)
+    reaching_definitions = reaching_defs_func(func)
+    dominance_tree, _ = build_dominance_tree(func)
+
+    for natural_loop in natural_loops:
+        loop_licm(natural_loop, cfg, preheadermap,
+                  reaching_definitions, dominance_tree)
+
     return join_cfg(cfg)
 
 
