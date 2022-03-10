bbb69fd73a8bf7d584b69be0ed201316d47abcc1
Jonathan Tran <jtran2718@gmail.com>
2022-03-09 20:06:35 -0500

    basic random instruction generation

--- random_bril_prog_gen.py
+++ random_bril_prog_gen.py
@@ -24,12 +24,18 @@ from random import randint
 from bril_core_constants import *
 
 
+MAIN = "main"
+
+
 MAX_INT = 2 ** 20
+MAX_INSTRS_PER_FUNC = 100
+MAX_FUNCS = 5
 
 RANDOM_VAR_PREFIX = "random_var"
 RANDOM_VAR_INDEX = 0
 
-RANDOM_LABEL_PREFIX = "random_label"
+RANDOM_LABEL_PREFIX = "random.label"
+RANDOM_LABEL_INDEX = 0
 
 
 def gen_op(bril_ops):
@@ -62,13 +68,43 @@ def gen_var():
     return f"{RANDOM_VAR_PREFIX}_{RANDOM_VAR_INDEX}"
 
 
-def gen_instr(bril_ops, bril_types, variables):
+def gen_label():
+    global RANDOM_LABEL_INDEX
+    RANDOM_LABEL_INDEX += 1
+    return f".{RANDOM_LABEL_PREFIX}.{RANDOM_LABEL_INDEX}"
+
+
+def exists_var_type(variables, typ):
+    vars_of_correct_typ = []
+    for v, t in variables:
+        if t == typ:
+            vars_of_correct_typ.append(v)
+    return vars_of_correct_typ != []
+
+
+def choose_var(variables, typ):
+    vars_of_correct_typ = []
+    for v, t in variables:
+        if t == typ:
+            vars_of_correct_typ.append(v)
+    randi = randint(0, len(vars_of_correct_typ) - 1)
+    return vars_of_correct_typ[randi]
+
+
+def choose_label(labels):
+    randi = randint(0, len(labels) - 1)
+    return labels[randi]
+
+
+def gen_instr(bril_ops, bril_types, variables, labels):
     """
-    Generates Random Instruction
+    Generates Random Instruction or Returns None if it cannot be generated
+    in the circumstances
     """
     assert type(bril_ops) == list
     assert type(bril_types) == list
     assert type(variables) == list
+    assert type(labels) == list
 
     op = gen_op(bril_ops)
     typ = gen_typ(bril_types)
@@ -80,36 +116,113 @@ def gen_instr(bril_ops, bril_types, variables):
             return (var, BOOL), {DEST: var, TYPE: BOOL, OP: CONST, VALUE: gen_bool()}
         raise RuntimeError(f"Unmatched Bril Type {typ}")
     elif op in [ADD, SUB, MUL, DIV]:
-        pass
+        if not exists_var_type(variables, INT):
+            return None
+        var1 = choose_var(variables, INT)
+        var2 = choose_var(variables, INT)
+        new_var = gen_var()
+        output_var = choose_var(variables + [(new_var, INT)], INT)
+        return (output_var, INT), {DEST: output_var, TYPE: INT, OP: op, ARGS: [var1, var2]}
     elif op in [EQ, LT, GT, LE, GE]:
-        pass
+        if not exists_var_type(variables, INT):
+            return None
+        var1 = choose_var(variables, INT)
+        var2 = choose_var(variables, INT)
+        new_var = gen_var()
+        output_var = choose_var(variables + [(new_var, BOOL)], BOOL)
+        return (output_var, BOOL), {DEST: output_var, TYPE: BOOL, OP: op, ARGS: [var1, var2]}
     elif op == NOT:
-        pass
+        if not exists_var_type(variables, BOOL):
+            return None
+        var1 = choose_var(variables, BOOL)
+        new_var = gen_var()
+        output_var = choose_var(variables + [(new_var, BOOL)], BOOL)
+        return (output_var, BOOL), {DEST: output_var, TYPE: BOOL, OP: op, ARGS: [var1]}
     elif op in [AND, OR]:
-        pass
+        if not exists_var_type(variables, BOOL):
+            return None
+        var1 = choose_var(variables, BOOL)
+        var2 = choose_var(variables, BOOL)
+        new_var = gen_var()
+        output_var = choose_var(variables + [(new_var, BOOL)], BOOL)
+        return (output_var, BOOL), {DEST: output_var, TYPE: BOOL, OP: op, ARGS: [var1, var2]}
     elif op == JMP:
-        pass
+        if labels == []:
+            return None
+        label = choose_label(labels)
+        return (None, None), {OP: JMP, LABELS: [label]}
     elif op == BR:
-        pass
+        if labels == []:
+            return None
+        label1 = choose_label(labels)
+        label2 = choose_label(labels)
+        return (None, None), {OP: BR, LABELS: [label1, label2]}
     elif op == RET:
-        pass
+        return None
+    elif op == LABEL:
+        label = gen_label()
+        return (LABEL, label), {LABEL: label}
     elif op == CALL:
-        pass
+        # TODO NOTE FIXME HACK BUG
+        return None
     elif op == ID:
-        pass
+        if not exists_var_type(variables, typ):
+            return None
+        var1 = choose_var(variables, typ)
+        new_var = gen_var()
+        output_var = choose_var(variables + [(new_var, typ)], typ)
+        return (output_var, typ), {DEST: output_var, TYPE: typ, OP: op, ARGS: [var1]}
     elif op == PRINT:
-        pass
+        if not exists_var_type(variables, typ):
+            return None
+        var1 = choose_var(variables, typ)
+        return (None, None), {OP: PRINT, ARGS: [var1]}
     elif op == NOP:
-        pass
+        return (None, None), {OP: NOP}
     elif op == PHI:
-        pass
+        # DO NOT HANDLE PHI NODES
+        return None
     raise RuntimeError(f"Unmatched Bril Instruction Operation {op}")
+
+
+def gen_function(name):
+    """
+    Generates a function
+    """
+    variables = []
+    labels = []
+    instrs = []
+    num_instrs = randint(1, MAX_INSTRS_PER_FUNC)
+    i = num_instrs
+    while i > 0:
+        result = gen_instr(BRIL_CORE_INSTRS +
+                           [LABEL], BRIL_CORE_TYPES, variables, instrs)
+        if result != None:
+            i -= 1
+            (var, typ), instr = result
+            if var == None:
+                pass
+            elif var == LABEL:
+                labels.append(typ)
+            else:
+                variables.append((var, typ))
+            instrs.append(instr)
+    return instrs
+
+
+def gen_program():
+    num_functions = randint(1, MAX_FUNCS)
+    for i in range(num_functions):
+        if i == 0:
+            instrs = gen_function(MAIN)
+            print(instrs)
+    return dict()
 
 
 @click.command()
 @click.option('--pretty-print', default=False, help='Pretty Print Randomly Generated Program.')
 def main(pretty_print):
-    prog = dict()
+    prog = gen_program()
     if pretty_print:
         print(json.dumps(prog, indent=4, sort_keys=True))
     print(json.dumps(prog))
