cf2e49b3157391a48bc209e0ea74b3a0acd9d1c1
Jonathan Tran <jtran2718@gmail.com>
2022-03-17 00:00:33 -0400

    stubs for ive

--- induction_variables.py
+++ induction_variables.py
@@ -8,14 +8,16 @@ import json
 import click
 
 
-def pass_routine(program):
-    for func in program["functions"]:
-        for instr in func["instrs"]:
-            pass
+def func_induction_variables(): pass
+
+
+def induction_varaibles(): pass
 
 
 @click.command()
-def main():
+@click.option('--pretty-print', default=False, help='Pretty Print Original Program.')
+@click.option('--ive', default=False, help='Pretty Print Original Program.')
+def main(pretty_print, ive):
     prog = json.load(sys.stdin)
     print(json.dumps(prog, indent=4, sort_keys=True))
     print(json.dumps(prog, indent=4, sort_keys=True))
