c9de9c03c60ab257474df268ad54a0035ee046a2
Jonathan Tran <jtran2718@gmail.com>
2022-04-04 02:06:28 -0500

    add loop fusion tests

--- loop_fusion.py
+++ loop_fusion.py
@@ -33,7 +33,8 @@ def loop_fuse(prog):
 
 @click.command()
 @click.option('--pretty-print', default=False, help='Pretty Print Original Program.')
-def main(pretty_print):
+@click.option('--fusion', default=False, help='Perform Loop Fusion.')
+def main(fusion, pretty_print):
     prog = json.load(sys.stdin)
     if pretty_print:
         print(json.dumps(prog, indent=4, sort_keys=True))
