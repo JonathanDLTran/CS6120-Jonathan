1a8b87a042f7ecabc3e73d9c86821fd120d669ae
Jonathan Tran <jtran2718@gmail.com>
2022-04-07 13:22:20 -0500

    interpreting alias analysis output issues

--- alias_analysis.py
+++ alias_analysis.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 
 from worklist_solver import Worklist
 
-from cfg import form_cfg_w_blocks
+from cfg import form_cfg, form_block_dict, form_blocks
 
 from bril_core_constants import *
 from bril_core_utilities import *
@@ -36,7 +36,7 @@ def merge(variables_lst):
 
 def transfer(variables, block):
     new_variables = deepcopy(variables)
-    for instr in block[INSTRS]:
+    for instr in block:
         if is_const(instr):
             new_variables[instr[DEST]] = set()
         elif is_id(instr):
@@ -55,26 +55,59 @@ def init_all_vars(func):
 
 
 def func_alias_analysis(func):
-    cfg = form_cfg_w_blocks(func)
-    variables = init_all_vars(func)
+    cfg = form_cfg(func)
+    assert len(cfg) != 0
+    entry = list(cfg.items())[0][0]
+    blocks = form_block_dict(form_blocks(func["instrs"]))
+    init = init_all_vars(func)
+    if ARGS in func:
+        args = func[ARGS]
+        for a in args:
+            init[a[NAME]] = set()
+    worklist = Worklist(entry, cfg, blocks, init, merge, transfer)
+    return worklist.solve()
 
 
-def alias_analysis():
-    pass
+def alias_analysis(prog):
+    for func in prog[FUNCTIONS]:
+        (in_dict, out_dict) = func_alias_analysis(func)
 
+        final_in_dict = OrderedDict()
+        for (key, inner_set) in in_dict.items():
+            inner_lst = list(
+                sorted([pair for pair in inner_set], key=lambda pair: pair[0]))
+            final_in_dict[key] = inner_lst
+        final_out_dict = OrderedDict()
+        for (key, inner_set) in out_dict.items():
+            inner_lst = list(
+                sorted([pair for pair in inner_set], key=lambda pair: pair[0]))
+            final_out_dict[key] = inner_lst
 
-def main():
-    pass
+        print(f"Function: {func[NAME]}")
+        print(f"In:")
+        for (k, v) in final_in_dict.items():
+            if v == []:
+                print(f"\t{k}: No Reaching Definitions.")
+            else:
+                for var in v:
+                    print(f"\t{k}: {var}.")
+        print(f"Out:")
+        for (k, v) in final_out_dict.items():
+            if v == []:
+                print(f"\t{k}: No Reaching Definitions.")
+            else:
+                for var in v:
+                    print(f"\t{k}: {var}.")
+    return
 
 
 @click.command()
 @click.option('--pretty-print', default=False, help='Pretty Print Original Program.')
-@click.option('--aa', default=False, help='Perform Alias Analysis.')
-def main(fusion, pretty_print):
+def main(pretty_print):
     prog = json.load(sys.stdin)
     if pretty_print:
         print(json.dumps(prog, indent=4, sort_keys=True))
-    pass
+    alias_analysis(prog)
 
 
 if __name__ == "__main__":
