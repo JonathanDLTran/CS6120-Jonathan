5dbbef7f57f1a8e866297c9e67271ac7b145f3a9
Jonathan Tran <jtran2718@gmail.com>
2022-05-18 19:34:25 -0400

    implementation detials from SLP paper

--- slp.py
+++ slp.py
@@ -1,10 +1,28 @@
 """
 Implementation of Larsen and Amarasingh's SLP paper from PLDI '00
+
+I've chosen a bit of a different approach here I'm going to identify longest
+runs of code that are isomorphic and independent
+
+From that longest run, I go forward and backward usign use-def and def-use
+chains as Larsen and Amarasingh do.
+
+Then I generate vector instructions for these packs. The packs are generated in a 
+topological order. There is guaranteed to be a topological ordering assuming there
+are no phi-nodes.
+
+I am going to try to use a vector pack as many times as possible, to avoid loading
+and unloading to and from vector packs. This is the main difference when compared
+to naive vectorization, which does not attempt to reuse packs at all.
 """
 
 import click
 import sys
 import json
+
+
+def topological_sort():
+    pass
 
 
 def slp_prog(prog):
@@ -21,6 +39,7 @@ def main(pretty_print):
     if pretty_print:
         print(json.dumps(final_prog, indent=4, sort_keys=True))
     print(json.dumps(final_prog))
+
 
 if __name__ == "__main__":
     main()
